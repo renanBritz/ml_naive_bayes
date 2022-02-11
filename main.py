fcbda2d3241d520aee11aeca7b0e65d7313ec9f3
Renan Britz <renanpiresbritz@hotmail.com>
2022-02-10 23:14:49 -0300

    add experiments to main

--- main.py
+++ main.py
@@ -5,9 +5,25 @@ from naive_bayes import NaiveBayes
 def main():
     data = load_csv('./data.csv')
     x, y = split_data(data, 'target')
-    nb = NaiveBayes()
-    nb.fit(x, y)
-    print(nb.predict(['low', 'low', 'low']))
+    model = NaiveBayes()
+    model.fit(x, y)
+    classes = model.get_classes()
+
+    print('Questionário Item B:')
+    print('A priori:', model.a_priori)
+
+    print('\nQuestionário Item C:')
+    print('P(price=med|target=acc) =', model.P(
+        attr=0, val='med', target='acc'))
+
+    x_test = ['low', 'small', 'high']
+    print('\nQuestionário item D')
+    print('Probabilidades:', model.get_a_posteriori(x_test))
+    print('Classe prevista:', classes[model.predict(x_test)])
+
+    print('\nQuestionário item E')
+    print('P(safety=low|target=acc) =', model.P(
+        attr=2, val='low', target='acc'))
 
 
 if __name__ == '__main__':
